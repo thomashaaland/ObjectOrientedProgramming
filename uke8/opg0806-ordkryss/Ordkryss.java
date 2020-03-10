ffe025002f5d34ba5b17dbb6f06671e6b5c75bd1
thomashaaland <thomas.haaland@gmail.com>
2020-03-10 15:44:07 +0100

    Finished Ordkryss.java

--- uke8/opg0806-ordkryss/Ordkryss.java
+++ uke8/opg0806-ordkryss/Ordkryss.java
@@ -1,13 +1,51 @@
 class Ordkryss {
+
+    // CheckAdjacent checks if the firstletter is in the current grid. If its
+    // not, return false. If it is found it checks if there is more letters.
+    // if there are no more letters, return true. Else checks neighbours.
+    public static boolean checkAdjacent(String letters, char[][] brett, int i, int j) {
+	int forwardI = (i + 1) % 4;
+	int backI = (4 + i - 1) % 4;
+	int forwardJ = (j + 1) % 4;
+	int backJ = (4 + j - 1) % 4;
+	char firstLetter = letters.charAt(0);
+	if (firstLetter == brett[i][j] && letters.length() == 1) {
+	    return true;
+	} else if (firstLetter != brett[i][j]) {
+	    return false;
+	} else {
+	    String rest = letters.substring(1);
+	    return checkAdjacent(rest, brett, backI, backJ) ||
+		checkAdjacent(rest, brett, backI, j) ||
+		checkAdjacent(rest, brett, backI, forwardJ) ||
+		checkAdjacent(rest, brett, i, backJ) ||
+		checkAdjacent(rest, brett, i, j) ||
+		checkAdjacent(rest, brett, i, forwardJ) ||
+		checkAdjacent(rest, brett, forwardI, backJ) ||
+		checkAdjacent(rest, brett, forwardI, j) ||
+		checkAdjacent(rest, brett, forwardI, forwardJ);
+	}
+    }
+
     public static boolean ordkryss(String word, char[][] brett) {
-	
+	for (int i = 0; i < 4; i++) {
+	    for (int j = 0; j < 4; j++) {
+		if (checkAdjacent(word, brett, i, j)) {
+		    return true;
+		}
+	    }
+	}
+	return false;
     }
     
     public static void main(String[] args) {
-	char [][] brett = {{"H", "A", "B", "D"},
-			   {"M", "V", "Q", "K"},
-			   {"P", "L", "U", "G"},
-			   {"A", "S", "R", "D"}};
+	// Brett is the playing board,
+	char [][] brett = {{'H', 'A', 'B', 'D'},
+			   {'M', 'V', 'Q', 'K'},
+			   {'P', 'L', 'U', 'G'},
+			   {'A', 'S', 'R', 'D'}};
 	
-	System.out.println(ordkryss("KURS", brett));
+	System.out.println("KURS: " + ordkryss("KURS", brett));
+	System.out.println("PAVE: " + ordkryss("PAVE", brett));
+    }
 }
